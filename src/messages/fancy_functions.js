645c30fc9062e40f03610a10920de5f630262382
Ethorbit <hi1212003@gmail.com>
2021-10-23 22:33:22 -0700

    Add cancel ability for fancy creation

--- src/messages/fancy_functions.js
+++ src/messages/fancy_functions.js
@@ -1,6 +1,8 @@
 const BotFunctions = require("../bot_functions.js");
 const Colors = require("../messages/colors.js");
 
+const CancelText = "The fancy sticky process has cancelled."
+
 function GetMessagePropertiesFromUser(msg, cb) // Gets the values from a user required to create a Fancy Sticky
 {
     try
@@ -10,8 +12,15 @@ function GetMessagePropertiesFromUser(msg, cb) // Gets the values from a user re
         let message;
     
         // Get color
+        msg.channel.send("Enter 'cancel' if you change your mind.");
         BotFunctions.SimpleMessage(msg.channel, "What hex color do you want? Example: #FFF68F (There are plenty of online tools to get that. Enter nocolor to use default.)", "What Color?", Colors["question"], sentMessage => {
             BotFunctions.WaitForUserResponse(msg.channel, msg.member, 300000, response => {
+                if (response.content == "cancel")
+                {
+                    BotFunctions.SimpleMessage(msg.channel, CancelText, "Cancelled", Colors["success"]);
+                    return;
+                }
+                
                 BotFunctions.DeleteMessage(sentMessage);
                 hex_color = response.content != "nocolor" ? response.content : "#FFF043";
     
@@ -19,6 +28,12 @@ function GetMessagePropertiesFromUser(msg, cb) // Gets the values from a user re
                 BotFunctions.SimpleMessage(msg.channel, "What should the title be? (Enter notitle to skip)", "What Title?", Colors["question"], sentMessage => {
                     BotFunctions.WaitForUserResponse(msg.channel, msg.member, 300000, response => {
                         BotFunctions.DeleteMessage(sentMessage);
+
+                        if (response.content == "cancel")
+                        {
+                            BotFunctions.SimpleMessage(msg.channel, CancelText, "Cancelled", Colors["success"]);
+                            return;
+                        }
     
                         if (response.content != "notitle")
                             title = response.content;
@@ -26,6 +41,12 @@ function GetMessagePropertiesFromUser(msg, cb) // Gets the values from a user re
                         // Get message
                         BotFunctions.SimpleMessage(msg.channel, "What should the message be?", "What Message?", Colors["question"], sentMessage => {
                             BotFunctions.WaitForUserResponse(msg.channel, msg.member, 600000, response => {
+                                if (response.content == "cancel")
+                                {
+                                    BotFunctions.SimpleMessage(msg.channel, CancelText, "Cancelled", Colors["success"]);
+                                    return;
+                                }
+                                            
                                 BotFunctions.DeleteMessage(sentMessage);
                                 
                                 message = response.content;    
